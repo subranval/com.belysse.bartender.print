c488a3a01502856b81aa7617de7c406ed719a9ee
Suman Branval <subranval@deloitte.com>
2025-08-29 11:13:09 +0000

    initial commit

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Filter"
+], (Controller, JSONModel, Fragment,Filter) => {
     "use strict";
 
     return Controller.extend("com.belysse.bartender.print.controller.Home", {
@@ -24,6 +26,57 @@ sap.ui.define([
             oViewModel.setProperty("/LabelTypeData", oLabelTypeData);
             oViewModel.setProperty("/DocTypeData", oDocTypeData);
 
+        },
+        openPlantVH: function () {
+            this.getView().setBusy(true);
+            if (!this._pPlantVHDialog) {
+                this._pPlantVHDialog = Fragment.load({
+                    id: this.getView().getId(),
+                    name: "com.belysse.bartender.print.view.fragments.PlantVH",
+                    controller: this
+                }).then(function (oDialog) {
+                    this.getView().addDependent(oDialog);
+                    return oDialog;
+                }.bind(this));
+            }
+            this._pPlantVHDialog.then(function (oDialog) {
+                oDialog.open();
+                this.getView().setBusy(false);
+            }.bind(this));
+        },
+        onSelectPlant: function (oEvent) {
+            let oTable = this.getView().byId("idPlantTable").getTable();
+            let oSelectedObj = oTable.getSelectedItem().getBindingContext().getObject();
+            let oViewModel = this.getView().getModel("oViewModel");
+            oViewModel.setProperty("/Plant", oSelectedObj.Plant);
+            this.getView().byId("idPlantTable").getParent().close();
+        },
+        onClosePlant: function (oEvent) {
+            oEvent.getSource().getParent().close();
+        },
+        openDocumentNoVH: function () {
+            let oView = this.getView();
+            if (!this._pValueHelpDialog) {
+                this._pValueHelpDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "com.belysse.bartender.print.view.fragments.DocumentNoVH",
+                    controller: this
+                }).then(function (oValueHelpDialog) {
+                    oView.addDependent(oValueHelpDialog);
+                    return oValueHelpDialog;
+                });
+            }
+
+            this._pValueHelpDialog.then(function (oValueHelpDialog) {
+                // create a filter for the binding
+                oValueHelpDialog.getBinding("items").filter([new Filter(
+                    "DocumentType",
+                    "EQ",
+                    "EL"
+                )]);
+                // open value help dialog filtered by the input value
+                oValueHelpDialog.open(sInputValue);
+            });
         }
     });
 });
